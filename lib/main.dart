d14d370d6a540e71d2307411d518447faaca7cc9
beinjamin <jauresbeinjamin@gmail.com>
2021-12-10 12:38:01 +0100

    Steless

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends class  extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
