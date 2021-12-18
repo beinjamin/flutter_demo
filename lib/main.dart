6343da7da26fb879ad8e68e5c5176146cf47d629
beinjamin <jauresbeinjamin@gmail.com>
2021-12-18 17:09:38 +0100

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class MyDrawer extends StatelessWidget {
       children: [
         DrawerHeader(
             child: CircleAvatar(
-          backgroundImage: AssetImage("images/test.png"),
+          backgroundImage: AssetImage("images/logo.png"),
         ))
       ],
     ));
