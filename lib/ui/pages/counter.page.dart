86e8a620a74d9be0e4e6eb11197e72cd1102fca1
beinjamin <jauresbeinjamin@gmail.com>
2021-12-25 19:03:04 +0100

    Convention en stateflu

--- lib/ui/pages/counter.page.dart
+++ lib/ui/pages/counter.page.dart
@@ -3,8 +3,14 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 // ignore: must_be_immutable
-class CounterPage extends StatelessWidget {
+class CounterPage extends StatefulWidget {
+  @override
+  _CounterPageState createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
   int counter = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,7 +19,7 @@ class CounterPage extends StatelessWidget {
         ),
         body: Center(
           child: Text(
-            'Counter Value => ${counter}',
+            'Counter Value => ${counter}%',
             style: TextStyle(fontSize: 22, color: Colors.deepOrange),
           ),
         ),
