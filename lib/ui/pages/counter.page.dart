27f60d81a85cf86291ac8796ee86731bd2c6e548
beinjamin <jauresbeinjamin@gmail.com>
2021-12-25 19:20:58 +0100

    Ajout du button remove

--- lib/ui/pages/counter.page.dart
+++ lib/ui/pages/counter.page.dart
@@ -9,24 +9,43 @@ class CounterPage extends StatefulWidget {
 }
 
 class _CounterPageState extends State<CounterPage> {
-  int counter = 0;
+  int counter = 50;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Counter'),
+      appBar: AppBar(
+        title: Text('Counter'),
+      ),
+      body: Center(
+        child: Text(
+          'Counter Value => ${counter}%',
+          style: TextStyle(fontSize: 22, color: Colors.deepOrange),
         ),
-        body: Center(
-          child: Text(
-            'Counter Value => ${counter}%',
-            style: TextStyle(fontSize: 22, color: Colors.deepOrange),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            child: Icon(Icons.remove),
+            onPressed: () {
+              setState(() {
+                ++counter;
+              });
+              print(counter);
+            },
           ),
-        ),
-        floatingActionButton: FloatingActionButton(
+          FloatingActionButton(
             child: Icon(Icons.add),
             onPressed: () {
+              setState(() {
+                ++counter;
+              });
               print(counter);
-            }));
+            },
+          )
+        ],
+      ),
+    );
   }
 }
